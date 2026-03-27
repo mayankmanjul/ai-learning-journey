df718fb2b0d402a9e0c98e8ab86574e80408b946
Mayank Shrivastava <mayank1322001@gmail.com>
2026-03-27 15:20:51 +0530

    string operation_indexing_len_function

--- phase-1-python/basics/Indexing.py
+++ phase-1-python/basics/Indexing.py
@@ -3,14 +3,9 @@ str1 =  'mayank shrivastava'
 str2 = "Anaya"
 str3 = """AI journey"""
 str4 = "AI is next revolution.\n big thing is coming on the way."
-# concat stings
-final_str = str1 + "\n" + str2 + "\n" + str3
-print(final_str)
-print(str1)
-print(str2)
-print(str3)
-print(str4)
-# len function
-print(len(str4))
+# Indexing
+ch = str1[0]
+print(ch)
 print(len(str2))
+
 
---
+++ phase-1-python/basics/Indexing_slicing.py
@@ -0,0 +1,20 @@
+# --printing string
+str4 = "AI is next revolution.\n big thing is coming on the way."
+# slicing - str[strating_idx : ending_idx] # ending_idx is not included
+ch = str4[6:10]
+ch1 = str4[0:10]
+ch2 = str4[0:len(str4)]
+ch3 = str4[0:]
+ch4 = str4[:len(str4)]
+ch5 = str4[-6:-1] # backward strat from -1
+ch6 = str4[-10:-1] # backward strat from -1
+ch7 = str4[-len(str4):-10]
+print("ch",ch)
+print("ch1",ch1)
+print("ch2",ch2)
+print("ch3",ch3)
+print("ch4",ch4)
+print("ch5",ch5)
+print("ch6",ch6)
+print("ch7",ch7)
+
